279c9f3d890799ab78d48f40c2641f02f19254b6
Yashtagad12 <tagadyash12@gmail.com>
2026-07-29 18:48:27 +0530

    First commit with navbar creation

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Navbar from './Components/Navbar';
+
+
+const App = () => {
+
+
+
+  return (
+    <>
+      <Navbar />
+    </>
+  )
+}
+
+export default App;
---
+++ src/Theme.js
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/material";
+
+
+export const theme = createTheme({
+    palette: {
+        mode: 'light',
+        primary: {
+            main: '#FF0000',
+        },
+        secondary: {
+            main: '#017BF5'
+        }
+    }
+})
