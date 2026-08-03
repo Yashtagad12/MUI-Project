e6589935e1ac8a9876abf97eca2238285135552f
Yashtagad12 <tagadyash12@gmail.com>
2026-08-03 18:05:19 +0530

    Added onclick functionality for sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
-
+import React from 'react';
 import './App.css'
 import Navbar from './Components/Navbar';
+import Sidebar from './Components/Sidebar';
 
 
 const App = () => {
 
-
+  const [sidebarOpen, setSidebarOpen] = React.useState(false);
 
   return (
     <>
-      <Navbar />
+      <Navbar onMenuClick={() => setSidebarOpen(true)} />
+
+      <Sidebar
+        open={sidebarOpen}
+        onClose={() => setSidebarOpen(false)}
+      />
     </>
   )
 }
